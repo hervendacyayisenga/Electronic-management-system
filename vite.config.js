ffbe587a82d4b3878d1b29ac60a99951ac8480b0
Pascal Ndizeye <pascal@example.com>
2026-03-14 18:28:56 +0200

    chore: configuration setup for fast github-pages deployments

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+    base: '/Electronic-management-system/',
     plugins: [vue()],
     resolve: {
         alias: {
